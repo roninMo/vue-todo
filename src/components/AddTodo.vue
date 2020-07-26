7fbdccfdc16b164cffa792b65d9a5f961bbf5fac
Kieran Schwegman <schwegmank@gmail.com>
2020-07-25 21:42:08 -0400

    Created restful crud app to learn vue fundamentals and connected it to a backend api

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,45 @@
+<template>
+  <form @submit.prevent="addTodo">
+    <input type="text" v-model="title" name="title" placeholder="Enter todo" />
+    <input type="submit" value="Submit" class="btn" />
+  </form>
+</template>
+
+<script>
+// import uuid from "uuid";
+
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      const newTodo = {
+        title: this.title,
+        completed: false,
+      };
+      // Send up to parent $emit functions
+      this.$emit("add-todo", newTodo);
+      this.title = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+
+input[type="submit"] {
+  flex: 2;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      This is a todo list app v1.0.0, it goes through some vue fundamentals, and
+      full crud functionality via axios. I've got a lot of experience in Angular
+      and React, so this was pretty easy to get a grasp on. Check out my Studio
+      Ghibli api for some nice css, I wanted to learn the functionality here.
+    </p>
+  </div>
+</template>
